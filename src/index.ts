c3669b24a433a1a06071e621f3ab3a72a4b36477
Hituzi Ando <hhh.ando@gmail.com>
2023-10-18 15:16:12 +0900

    Moved Dictionary.ts to type

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export * from './identifier/UUID';
 export * from './message_digest/messageDigest';
 export * from './type/BaseError';
 export * from './type/Result';
-export * from './Dictionary';
+export * from './type/Dictionary';
 export * from './L10n';
 export * from './Logger';
