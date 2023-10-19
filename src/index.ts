285cfaa34d47c935f0094687fe7a4f694f65b1c1
Hituzi Ando <hhh.ando@gmail.com>
2023-10-19 14:22:26 +0900

    Moved L10n.ts to l10n/ and Logger.ts to logger/

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ export * from './message_digest/messageDigest';
 export * from './type/BaseError';
 export * from './type/Result';
 export * from './type/Dictionary';
-export * from './L10n';
-export * from './Logger';
+export * from './l10n/L10n';
+export * from './logger/Logger';
