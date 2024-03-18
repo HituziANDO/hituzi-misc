a0bb4e1aefd06e7a431e7fa7ecef1c8247f2e453
Hituzi Ando <hhh.ando@gmail.com>
2024-03-18 22:59:32 +0900

    Fixed build.lib.name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     build: {
         lib: {
             entry: 'src/index.ts',
-            name: 'hituzi-misc',
+            name: 'HituziMisc',
             fileName: (format) => `index.${format}.js`
         },
         rollupOptions: {
